eb5ef37192f99c27885f27590353f87e02c537b4
JackQiu <qiuzhantong@qiyi.com>
2020-03-16 21:19:49 +0800

    leetcode question 557

---
+++ test/session1-lc557.test.js
@@ -0,0 +1,5 @@
+import reverseWords from '../code/session1-lc557'
+
+test('Let\'s take LeetCode contest reverse to s\'teL ekat edoCteeL tsetnoc', () => {
+  expect(reverseWords('Let\'s take LeetCode contest')).toBe('s\'teL ekat edoCteeL tsetnoc')
+})
