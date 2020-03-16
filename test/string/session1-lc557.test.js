cb14b4f933c8e38c336417fc24ff8b2ceea129c2
JackQiu <qiuzhantong@qiyi.com>
2020-03-16 21:39:28 +0800

    update session1

--- test/string/session1-lc557.test.js
+++ test/string/session1-lc557.test.js
@@ -1,4 +1,4 @@
-import reverseWords from '../code/session1-lc557'
+import reverseWords from '../../code/string/session1-lc557'
 
 test('Let\'s take LeetCode contest reverse to s\'teL ekat edoCteeL tsetnoc', () => {
   expect(reverseWords('Let\'s take LeetCode contest')).toBe('s\'teL ekat edoCteeL tsetnoc')
